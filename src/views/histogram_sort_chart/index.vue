c025c4c3d71d4e9944b5d664ee18e6675d821a75
caopeng <caopeng_404@163.com>
2025-03-26 09:30:52 +0800

    实现x轴调整

--- src/views/histogram_sort_chart/index.vue
+++ src/views/histogram_sort_chart/index.vue
@@ -58,29 +58,6 @@ onMounted(() => {
     .attr("fill", "steelblue")
     .attr("width", 0);
 
-  // 动态排序和更新柱状图
-  function update() {
-    // 对数据进行排序
-    const sortedData = data.slice().sort((a, b) => b - a);
-    const sortedLabels = label
-      .slice()
-      .sort((a, b) => data[label.indexOf(b)] - data[label.indexOf(a)]);
-
-    // 更新 Y 轴比例尺
-    y.domain(sortedLabels);
-
-    // 更新 Y 轴
-    yAxisG.call(d3.axisLeft(y));
-
-    // 更新柱状图
-    bars
-      .data(sortedData)
-      .transition()
-      .duration(1000)
-      .attr("y", (d, i) => y(sortedLabels[i]))
-      .attr("width", (d) => x(d));
-  }
-
   // 定时器函数，每隔 3 秒增加数据并更新图表
   function tick() {
     // 随机增加数据
@@ -90,7 +67,7 @@ onMounted(() => {
     // 更新 X 轴比例尺
     const x = d3
       .scaleLinear()
-      .domain([0, d3.max(data)])
+      .domain([0, d3.max(data)]) // 根据 data 中的最大值更新 x 轴的定义域
       .range([0, width]);
     // 更新 X 轴
     xAxisG.call(d3.axisBottom(x));
@@ -98,11 +75,36 @@ onMounted(() => {
     update();
   }
 
+  // 动态排序和更新柱状图
+  function update() {
+    // 对数据进行排序
+    const sortedData = data.slice().sort((a, b) => b - a);
+    const sortedLabels = label
+      .slice()
+      .sort((a, b) => data[label.indexOf(b)] - data[label.indexOf(a)]);
+    // 更新 Y 轴比例尺
+    y.domain(sortedLabels);
+    // 更新 Y 轴
+    yAxisG.call(d3.axisLeft(y));
+    // 更新 X 轴比例尺
+    const x = d3
+      .scaleLinear()
+      .domain([0, d3.max(data)]) // 根据 data 中的最大值更新 x 轴的定义域
+      .range([0, width]);
+    // 更新柱状图
+    bars
+      .data(sortedData)
+      .transition()
+      .duration(1000)
+      .attr("y", (d, i) => y(sortedLabels[i]))
+      .attr("width", (d) => x(d));
+  }
+
   // 初始更新
   update();
 
   // 启动定时器
-  timer = setInterval(tick, 3000);
+  timer = setInterval(tick, 1000);
 });
 
 // 组件卸载时清除定时器
