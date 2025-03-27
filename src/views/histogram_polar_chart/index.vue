98022b37a565e7b0721bdec924a8ce3050ff3536
caopeng <caopeng_404@163.com>
2025-03-27 09:17:05 +0800

    添加数据

--- src/views/histogram_polar_chart/index.vue
+++ src/views/histogram_polar_chart/index.vue
@@ -1,6 +1,28 @@
 <template>
   <svg></svg>
 </template>
-<script setup></script>
+<script setup>
+import { onMounted } from "vue";
+import * as d3 from "d3";
+const data = [
+  {
+    label: "a",
+    value: 2,
+  },
+  {
+    label: "b",
+    value: 1.2,
+  },
+  {
+    label: "c",
+    value: 2.4,
+  },
+  {
+    label: "d",
+    value: 3.6,
+  },
+];
+onMounted(() => {});
+</script>
 
 <style scoped></style>
